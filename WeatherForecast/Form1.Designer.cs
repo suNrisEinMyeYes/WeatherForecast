626998e011f4678ed2941c80be594299c26dae35
suNrisEinMyeYes <iklown3@gmail.com>
2020-06-05 19:49:47 +0700

    init

---
+++ WeatherForecast/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace WeatherForecast
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.DayNumbLbl = new System.Windows.Forms.Label();
+            this.DayPeriodLbl = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SunnyLbl = new System.Windows.Forms.Label();
+            this.CloudyLbl = new System.Windows.Forms.Label();
+            this.RainyLbl = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(22, 45);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "StartSimulations";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(182, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Day: ";
+            // 
+            // DayNumbLbl
+            // 
+            this.DayNumbLbl.AutoSize = true;
+            this.DayNumbLbl.Location = new System.Drawing.Point(233, 66);
+            this.DayNumbLbl.Name = "DayNumbLbl";
+            this.DayNumbLbl.Size = new System.Drawing.Size(13, 13);
+            this.DayNumbLbl.TabIndex = 2;
+            this.DayNumbLbl.Text = "0";
+            // 
+            // DayPeriodLbl
+            // 
+            this.DayPeriodLbl.AutoSize = true;
+            this.DayPeriodLbl.Location = new System.Drawing.Point(288, 66);
+            this.DayPeriodLbl.Name = "DayPeriodLbl";
+            this.DayPeriodLbl.Size = new System.Drawing.Size(0, 13);
+            this.DayPeriodLbl.TabIndex = 3;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(43, 133);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(118, 87);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(439, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Statistics";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(331, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Sunny";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(449, 140);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Cloudy";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(582, 140);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Rainy";
+            // 
+            // SunnyLbl
+            // 
+            this.SunnyLbl.AutoSize = true;
+            this.SunnyLbl.Location = new System.Drawing.Point(334, 180);
+            this.SunnyLbl.Name = "SunnyLbl";
+            this.SunnyLbl.Size = new System.Drawing.Size(35, 13);
+            this.SunnyLbl.TabIndex = 10;
+            this.SunnyLbl.Text = "label6";
+            // 
+            // CloudyLbl
+            // 
+            this.CloudyLbl.AutoSize = true;
+            this.CloudyLbl.Location = new System.Drawing.Point(449, 180);
+            this.CloudyLbl.Name = "CloudyLbl";
+            this.CloudyLbl.Size = new System.Drawing.Size(35, 13);
+            this.CloudyLbl.TabIndex = 11;
+            this.CloudyLbl.Text = "label7";
+            // 
+            // RainyLbl
+            // 
+            this.RainyLbl.AutoSize = true;
+            this.RainyLbl.Location = new System.Drawing.Point(585, 179);
+            this.RainyLbl.Name = "RainyLbl";
+            this.RainyLbl.Size = new System.Drawing.Size(35, 13);
+            this.RainyLbl.TabIndex = 12;
+            this.RainyLbl.Text = "label8";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(726, 304);
+            this.Controls.Add(this.RainyLbl);
+            this.Controls.Add(this.CloudyLbl);
+            this.Controls.Add(this.SunnyLbl);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.DayPeriodLbl);
+            this.Controls.Add(this.DayNumbLbl);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label DayNumbLbl;
+        private System.Windows.Forms.Label DayPeriodLbl;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label SunnyLbl;
+        private System.Windows.Forms.Label CloudyLbl;
+        private System.Windows.Forms.Label RainyLbl;
+    }
+}
+
---
+++ WeatherForecast/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WeatherForecast
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// 0 - Sunny, 1 - Cloudy, 2 -Rainy
+        /// </summary>
+        private Bitmap MyImage;
+        public Random rnd;
+        int DayControll = 0;
+        public float[,] Matrix_P =  { { 0.3f, 0.4f, 0.3f }, { 0.2f, 0.6f, 0.2f },{ 0.1f,0.4f,0.5f} };
+        public List<int> Freq = new List<int>();
+        public int CurrentState = 1;
+        public int StateToChange;
+        const string pathToRain = "Rain.jpg", pathToSunny = "Sunny.jpg", pathToCloudy = "Cloudy.jpg";
+        const int xSize = 100, ySize = 100;
+        public List<float> values = new List<float>();
+        public Form1()
+        {
+            InitializeComponent();
+            //timer1.Enabled = true;
+            timer1.Interval = 1000;
+            rnd = new Random();
+            for (int i = 0; i < 3; i++)
+            {
+                Freq.Add(0);
+            }
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            switch (DayControll)
+            {
+                case 0:
+                    DayPeriodLbl.Text = "Morning";
+                    DayControll++;
+                    break;
+                case 1:
+                    DayPeriodLbl.Text = "Day";
+                    DayControll++;
+                    break;
+                case 2:
+                    DayPeriodLbl.Text = "Evening";
+                    int DayNumb = Int32.Parse(DayNumbLbl.Text);
+                    DayNumb++;
+                    DayNumbLbl.Text = DayNumb.ToString();
+                    SunnyLbl.Text = ((float)Freq[0] / (float)DayNumb).ToString();
+                    CloudyLbl.Text = ((float)Freq[1] / (float)DayNumb).ToString();
+                    RainyLbl.Text = ((float)Freq[2] / (float)DayNumb).ToString();
+                    DayControll = 0;
+                    break;
+                default:
+                    break;
+            }
+            
+            
+            values.Clear();
+            for (int i = 0; i < 3; i++)
+            {
+                values.Add(Matrix_P[CurrentState, i]);
+                
+            }
+            StateToChange = filler();
+            Freq[StateToChange]++;
+            Console.WriteLine(StateToChange);
+            switch (StateToChange)
+            {
+                case 0:
+                    DisplayImage(pathToSunny);
+                    break;
+                case 1:
+                    DisplayImage(pathToCloudy);
+                    break;
+                case 2:
+                    DisplayImage(pathToRain);
+                    break;
+                default:
+                    break;
+            }
+
+            
+
+
+        }
+        int filler()
+        {
+            double A = 0;
+            int k = 0;
+            A = rnd.NextDouble();
+
+            k = 0;
+            foreach (var value in values)
+            {
+                A -= value;
+                if (A <= 0)
+                {
+
+                    break;
+                }
+                k++;
+            }
+
+            return k;
+        }
+
+        public void DisplayImage(String fileToDisplay)
+        {
+            // Sets up an image object to be displayed.
+            if (MyImage != null)
+            {
+                MyImage.Dispose();
+            }
+
+            // Stretches the image to fit the pictureBox.
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            MyImage = new Bitmap(fileToDisplay);
+            pictureBox1.ClientSize = new Size(xSize, ySize);
+            pictureBox1.Image = (Image)MyImage;
+        }
+        public void StatCollector()
+        {
+
+        }
+    }
+}
